122519b286aa186c4556503bfe1a8b2aa8754aad
thanhhaonguyen2310 <threshazir123@gmail.com>
2026-01-24 22:55:52 +0700

    fix: allow empty enum fields when importing excel

--- backend/src/constants/config.constants.js
+++ backend/src/constants/config.constants.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    TOKEN_EXPIRE_TIME: process.env.ACCESS_TOKEN_EXPIRE_TIME || '20s',
+    TOKEN_EXPIRE_TIME: process.env.ACCESS_TOKEN_EXPIRE_TIME || '1h',
     REFRESH_TOKEN_EXPRITE: process.env.REFRESH_TOKEN_EXPRITE || '30d'
 }
 
